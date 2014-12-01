b27dd4477349414ba4455cc22d4fa5ed96fe2882
Jeff Wirsing <jb-wirsing@wiu.edu>
2014-11-30 23:33:25 -0600

    Added image

--- BlockDefenderTutorial/GameScene.swift
+++ BlockDefenderTutorial/GameScene.swift
@@ -11,13 +11,13 @@ import SpriteKit
 class GameScene: SKScene {
     override func didMoveToView(view: SKView) {
         /* Setup your scene here */
-        let myLabel = SKLabelNode(fontNamed:"Chalkduster")
-        myLabel.text = "Hello, World!";
-        myLabel.fontSize = 65;
-        myLabel.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame));
-        
-        self.addChild(myLabel)
+        setupLevel()
     }
+    
+    func setupLevel(){
+        
+    }
+    
     
     override func touchesBegan(touches: NSSet, withEvent event: UIEvent) {
         /* Called when a touch begins */
